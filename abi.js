af133779eba897519f7044b2dfa768f4f20600d5
Zainan Victor Zhou <zzn+github@zzn.im>
2022-06-07 17:36:14 -0700

    Add events for ERC721

--- abi.js
+++ abi.js
@@ -1,6 +1,9 @@
 // License: CC0
 export const ERC721ABI = [
     /* --- ERC721 Core --- */
+    'event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId)',
+    'event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId)',
+    'event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved)',
     'function balanceOf(address owner) external view returns (uint256 balance)',
     'function ownerOf(uint256 tokenId) external view returns (address owner)',
     'function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external',
